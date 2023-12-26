7a746db8fe952536151b695578ae506ec7365ead
IshakAtes <108549234+IshakAtes@users.noreply.github.com>
2023-12-26 23:56:32 +0100

    startGame Button

--- src/app/start-screen/start-screen.component.ts
+++ src/app/start-screen/start-screen.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { GameComponent } from '../game/game.component';
 
 @Component({
   selector: 'app-start-screen',
   standalone: true,
-  imports: [],
+  imports: [GameComponent],
   templateUrl: './start-screen.component.html',
   styleUrl: './start-screen.component.scss'
 })
 export class StartScreenComponent {
+
+  constructor(private router: Router) { }
+
   newGame() {
-    // start Game
+    this.router.navigate(['/game']);
   }
 }
