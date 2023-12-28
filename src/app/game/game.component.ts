e40ba3e0a0a827c69040b76c5cdb873bffa4e1cb
IshakAtes <108549234+IshakAtes@users.noreply.github.com>
2023-12-28 23:07:29 +0100

    shuffle the stack array

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+import { Game } from '../../models/game';
 
 @Component({
   selector: 'app-game',
@@ -10,7 +11,18 @@ import { Component } from '@angular/core';
 })
 export class GameComponent {
   pickCardAnimation = false;
-  
+  game: Game;
+
+  constructor() {
+    this.game = new Game()
+    this.newGame();
+  }
+
+  newGame() {
+    this.game = new Game();
+    console.log(this.game);
+  }
+
   takeCard() {
     this.pickCardAnimation = true;
   }
