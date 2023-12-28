eb3110c2ac6f19ccb4e8a9b1e268fc17c1c6364c
IshakAtes <108549234+IshakAtes@users.noreply.github.com>
2023-12-28 23:24:05 +0100

    on every click show an other card

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -11,6 +11,7 @@ import { Game } from '../../models/game';
 })
 export class GameComponent {
   pickCardAnimation = false;
+  currentCard: string | any = '';
   game: Game;
 
   constructor() {
@@ -24,6 +25,15 @@ export class GameComponent {
   }
 
   takeCard() {
-    this.pickCardAnimation = true;
+    if (!this.pickCardAnimation) {
+      this.currentCard = this.game.stack.pop();
+      console.log(this.currentCard);
+      this.pickCardAnimation = true;
+
+      setTimeout(() => {
+        this.pickCardAnimation = false;
+      }, 1500);
+    }
   }
+
 }
