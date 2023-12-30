2c26e4d5a3094299d942cc9fd404419e875a73d3
IshakAtes <108549234+IshakAtes@users.noreply.github.com>
2023-12-31 00:21:39 +0100

    fixed ngModel problem

--- src/app/dialog-add-player/dialog-add-player.component.ts
+++ src/app/dialog-add-player/dialog-add-player.component.ts
@@ -1,19 +1,20 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import {MatDialogModule} from '@angular/material/dialog';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-dialog-add-player',
   standalone: true,
-  imports: [MatDialogModule, MatButtonModule, MatInputModule, MatFormFieldModule],
+  imports: [MatDialogModule, MatButtonModule, MatInputModule, MatFormFieldModule, FormsModule],
   templateUrl: './dialog-add-player.component.html',
   styleUrl: './dialog-add-player.component.scss'
 })
 export class DialogAddPlayerComponent {
-  name: any = '';
+  name: string = '';
 
   onNoClick() {
     console.log('hi');
